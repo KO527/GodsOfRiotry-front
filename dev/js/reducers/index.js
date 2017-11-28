a4e6518a584882766da0e7e8c55d6419360d5d2f
K0527 <Kwajo.oseiakoto@gmail.com>
2017-11-28 02:55:12 +0000

    added playlist reducer to common reducer

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,15 +1,16 @@
-import {combineReducers} from 'redux';
-import UserReducer from './reducer-users';
-import ActiveUserReducer from './reducer-active-user';
+i
+port {combineReducers} from 'redux';
 
 /*
  * We combine all reducers into a single object before updated data is dispatched (sent) to store
  * Your entire applications state (store) is just whatever gets returned from all your reducers
  * */
+import {reducer as formReducer} from 'redux-form';
+import PlaylistReducer from '../playlist_reducer';
 
 const allReducers = combineReducers({
-    users: UserReducer,
-    activeUser: ActiveUserReducer
+ form: formReducer, 
+ playlist: PlaylistReducer
 });
 
 export default allReducers
