0bc10046d38b05e62e931f3e2d72c577d8cb9fe0
K0527 <Kwajo.oseiakoto@gmail.com>
2017-11-28 03:06:53 +0000

    updated actions/index.js

--- dev/js/actions/index.js
+++ dev/js/actions/index.js
@@ -1,7 +1,23 @@
-export const selectUser = (user) => {
-    console.log("You clicked on user: ", user.first);
-    return {
-        type: 'USER_SELECTED',
-        payload: user
-    }
-};
+
+var SC = require('node-soundcloud');
+
+const API_URL = "http://localhost:5000/api/v1";
+
+export function getPlaylist(props){
+	const request = SC.get('me/playlists', props, {limit: 1});
+	
+	return{
+		type: GET_PLAYLIST,
+		payload: request
+	}
+}
+
+export function getInitialPlaylist(){
+	const request = axios.get('${API_URL}/soundcloud');
+		
+	return{
+	    type: INITIAL_PLAYLIST,
+	    payload: request
+	}
+}
+
