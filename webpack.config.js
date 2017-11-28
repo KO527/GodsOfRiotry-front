b8db6eed9fe6ab422eca769664de70553f48d63e
EC2 Default User <ec2-user@ip-172-31-95-155.ec2.internal>
2017-11-28 02:38:02 +0000

    updated configuration for app

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,25 @@
 var path = require('path');
 var webpack = require('webpack');
+var SRC = path.join(__dirname, 'dev/js/');
 
 module.exports = {
     devServer: {
         inline: true,
-        contentBase: './src',
+        contentBase: 'src',
         historyApiFallback: true,
+        public: 'ec2-54-164-27-229.compute-1.amazonaws.com',
+	host: '0.0.0.0',
         port: 8080
     },
     devtool: 'cheap-module-eval-source-map',
+    resolve: {
+        root: [SRC]
+    },
     entry: './dev/js/index.js',
     module: {
         loaders: [
             {
-                test: /\.js$/,
+                test: /\.jsx?$/,
                 loaders: ['babel'],
                 exclude: /node_modules/
             },
