9177eacc07c20a9f56e1853f24c1d32c78fc0fa4
K0527 <Kwajo.oseiakoto@gmail.com>
2017-12-03 07:08:50 +0000

    made progress to playlist component

--- dev/js/components/Playlist.js
+++ dev/js/components/Playlist.js
@@ -1,25 +1,47 @@
-export default class Playlist extends Component {
- 
-  var SC = require('node-soundcloud');  
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+var SC = require('node-soundcloud');
 
-  componentWillMount(){
+export default class Playlist extends Component {
+  
+  
+  componentDidMount(){
 	SC.initialize({
             client_id: ENV["SOUNDCLOUD_CLIENT_ID"],
             redirect_uri: "http://godsofriotry.com/playlist" 
         });
+
         this.props.getInitialPlaylist().then(function(playlist){
-		SC.oEmbed(playlist, element: document.getElementByClassName('media-play'));
+		SC.oEmbed(playlist, element: document.getElementByClassName('media_play'));
         });
   }
 
+  var connect = "<img src = '../../images/btn-connect-m.png'>";
+  var disconnect = "<img src = '../../images/btn-disconnect-m.png'>";
   
+  componentWillReceiveProps(nextProps){
+	if (this.props.term.playlistTitle !== nextProps.term.playlistTitle){
+	  	this.getPlaylist();
+	}
+	else{
+	   this.props.term.tracks.map((track) => function(track){
+	     if (!nextProps.term.tracks.include(track)){
+		this.getPlaylist();
+	     }
+	   }	
+	}
+   }
+		
+		
+	 
   connect(){
 	
 	SC.connect().then(function(){
 		return SC.get('/me');
 	}).then(function(me){
-		document.getElementsByClassName("connect-disconnect-container").innerHtml = "<img src = "../../images/btn-disconnect-m.png">";
-		return true
+		document.getElementsByClassName("connect_disconnect_container").innerHtml = disconnect;
+		this.importPlaylist();
+		return true;
 	}).catch(function(reason){
 		alert(reason);
 		return false;
@@ -27,22 +49,43 @@ export default class Playlist extends Component {
   }
  
   disconnect(){
-	
-	SC.initialize({oauth_token: null}).then(function(){
-		document.getElementsByClassName("connect_disconnect_container").innerHtml = "<img src="../../images/btn-connect-m.png">";
+	document.getElementsByClassName('media_play').innerHtml = "";
+	const {id} = this.props.match.params;
+	this.props.replacePlaylist(id);
+	this.props.getInitialPlaylist().then(function(playlist){
+         SC.oEmbed(playlist, element: document.getElementByClassName('media_play'));
+        }).then(function(){
+	    document.getElementsByClassName("connect_disconnect_container").innerHtml = connect;
 	});
   }
+  
+  getPlaylist(){
+	document.getElementByClassName('media_play').innerHtml = '';
+	return SC.get('me/${this.props.term.playlistTitle}').then(function(playlist){
+		SC.oEmbed(playlist, element: document.getElementByClassname('media_play'));
+	}
+  }
 
+  importPlaylist(){
+	this.props.term.playlistTitle = prompt('Which playlist do you wish to import?', 'Playlist Name:', 'Name:');
+	
+	this.getPlaylist().then(function(playlist){
+		SC.oEmbed(playlist, element: document.getElementByClassName('media_play'));
+	});
+  }
+  
   render(){
       return(
            <div className="PlaylistLayout">
                <header className= "media">
-                  <nav>
-		     <li className="media-play">
+        	  <div className "search-bar">
+			<input value = {this.state.term} onChange={event => this.onInputChange(event.target.value)} />
+                  </div>
+	          <nav>
+		     <li className="media_play">
                
 		     </li>
-		     <Link to = {connect() ? this.connect() : this.disconnect() }>
-		      <img src = "../../images/btn-connect-m.png" className="SoundCloudSignIn"/>
+		     <Link to = {connect() ? this.disconnect() : this.connect() } className = "connect_disconnect_container">
 		     </Link>
                   </nav>
                </header>
@@ -51,3 +94,10 @@ export default class Playlist extends Component {
   }
 		
 }
+
+function mapStateToProps(state){
+	return {playlist: state.playlist}
+}
+
+default connect(mapStateToProps, {replacePlaylist, getInitialPlaylist})(Playlist);
+
