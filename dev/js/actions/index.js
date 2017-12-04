019bedc7d644b49283af8b12bb5b0b1698e78d21
K0527 <Kwajo.oseiakoto@gmail.com>
2017-12-04 00:11:54 +0000

    updated actions

--- dev/js/actions/index.js
+++ dev/js/actions/index.js
@@ -3,15 +3,6 @@ var SC = require('node-soundcloud');
 
 const API_URL = "http://localhost:5000/api/v1";
 
-export function getPlaylist(props){
-	const request = SC.get('me/playlists', props, {limit: 1});
-	
-	return{
-		type: GET_PLAYLIST,
-		payload: request
-	}
-}
-
 export function getInitialPlaylist(){
 	const request = axios.get('${API_URL}/soundcloud');
 		
@@ -21,3 +12,11 @@ export function getInitialPlaylist(){
 	}
 }
 
+export function replaceInitialPlaylist(id){
+	const request = axios.delete('${API_URL}/soundcloud/${id}');
+
+	return {
+	   type: REPLACE_INITIAL_PLAYLIST,
+	   payload: request
+        }
+}
