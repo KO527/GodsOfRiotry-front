820fc0f22180a7d4f534d9125ef196038230b322
KO527 <Kwajo.oseiakoto@gmail.com>
2018-03-31 02:35:18 -0400

    dealt with configuration issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,69 @@
+var webpack = require('webpack');
+
  module.exports = {
    entry: './dev/js/index.js',
    output: {
      path: __dirname + '/src',
-     publicPath: '/',
-     filename: '/js/bundle.min.js'
+     publicPath: __dirname + '/src',
+     filename: './js/bundle.min.js'
    },
    module: {
-     loaders: [{
-       exclude: /node_modules/,
-       loader: 'babel-loader',
-       query: {
-         presets: ['react', 'es2015', 'stage-2'],
-         plugins: ['transform-decorators-legacy']
-       }
-     }],
-     rules: [{ test: /\.scss$/,
-                        use: [{loader: 'sass-loader'}, 
-                              {loader: 'css-loader'}, 
-                              {loader: 'style-loader'}]}]
+      rules: [
+         {
+           exclude: /node_modules/,
+           test: /\.js$/,
+           loader: 'babel-loader',
+           options: {
+             presets: ['react', 'es2015'],
+             plugins: ['transform-decorators-legacy', 'transform-decorators']
+           }
+         },
+         {
+           test: /\.(html)$/,
+           use: {
+              loader: 'html-loader',
+              options: {
+                attrs: [':data-src']
+              }
+           }
+         },
+         { test: /\.scss$/,
+                    exclude: '/node_modules/',
+                    use: [{
+                        loader: "style-loader" // creates style nodes from JS strings
+                    }, {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    }, {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }]
+         },
+         {test: /\.(png|jpg|gif)$/,
+          exclude: '/node_modules/',
+          use: [{ loader: 'file-loader',
+                  options: {
+                      publicPath: __dirname + '/dev/images',
+                      names: ['./btn-connect-m.png', './btn-disconnect-m.png']
+                    }
+                },
+                {
+                  loader: 'image-webpack-loader',
+                  options: {
+                      gifsicle: {
+                        optimizationLevel: 7,
+                        interlaced: false
+                      },
+                      optipng: {
+                        optimizationLevel: 7,
+                        interlaced: false
+                      }
+                  }
+                }]
+          }
+      ]
    },
    resolve: {
-     extensions: ['', '.js', '.jsx']
+     extensions: ['.js', '.jsx'],
+     modules: ['node_modules']
    },
    devServer: {
      historyApiFallback: true,
