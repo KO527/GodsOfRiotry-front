fb50f64d49e7ed3af458b9bf20bf7c16e0ebc184
KO527 <Kwajo.oseiakoto@gmail.com>
2018-07-09 23:41:38 -0400

    updated reducers

--- src/reducers/event_options_reducer.js
+++ src/reducers/event_options_reducer.js
@@ -1,20 +1,22 @@
-import { EVENTS_BY_TEAM, EVENTS_BY_ARTIST, SEARCH_TERM, IMMEDIATE_EVENTS, SPORTING_EVENTS } from '../actions/types';
+import { EVENTS_BY_TEAM, EVENTS_BY_ARTIST, SEARCH_TERM, IMMEDIATE_EVENTS, SPECIFIC_EVENTS, SPORTING_EVENTS } from '../actions/types';
 
 const initialState = {
 	imm_events: [],
 	team_events: [],
-	artists_events: [],
+	artist_events: [],
 	queried_events: []
 }
 
 export default function(state = initialState, action){
 	switch(action.type){
 		case IMMEDIATE_EVENTS:
-			return {...state, imm_events: action.payload.events}
+			return {...state, imm_events: action.payload}
 		case SPORTING_EVENTS:
-			return {...state, team_events: action.payload.events}
+			return {...state, team_events: action.payload}
+		case SPECIFIC_EVENTS:
+			return {...state, specific_team_events: action.payload}
 		case EVENTS_BY_ARTIST:
-			return {...state, artists_events: action.payload.events}
+			return {...state, artist_events: action.payload}
 		case SEARCH_TERM: 
 			return {...state, queried_events: action.payload.events}
 		default: 
--- src/reducers/possible_matches_reducer.js
+++ src/reducers/possible_matches_reducer.js
@@ -16,25 +16,25 @@ export default function(state = initialState, action){
 	
 	switch(action.type){
 		case INITIAL_PIECES:
-			return Object.assign({}, state, {contemplated_piece: action.payload.contemplated_piece},
-										    {extraTops: action.payload.extra_tops},
-				                            {extraBottoms: action.payload.extra_bottoms},
-				                            {standaloneTops: action.payload.standalone_tops},
-				                            {standaloneBottoms: action.payload.standalone_bottoms},
-				                            {suggestedTops: action.payload.suggested_tops},
-				                            {suggestedBottoms: action.payload.suggested_bottoms})
+			return {...state, contemplated_piece: action.payload.contemplated_piece,
+							  extraTops: action.payload.extra_tops,
+	                          extraBottoms: action.payload.extra_bottoms,
+	                          standaloneTops: action.payload.standalone_tops,
+	                          standaloneBottoms: action.payload.standalone_bottoms,
+	                          suggestedTops: action.payload.suggested_tops,
+	                          suggestedBottoms: action.payload.suggested_bottoms}
 		case GET_ANCILLARY_PIECES:
-		   return Object.assign({}, state, {extraTops: action.payload.extra_tops},
-				                           {extraBottoms: action.payload.extra_bottoms},
-				                           {standaloneTops: action.payload.standalone_tops},
-				                           {standaloneBottoms: action.payload.standalone_bottoms},
-				                           {suggestedTops: action.payload.suggested_tops},
-				                           {suggestedBottoms: action.payload.suggested_bottoms})
+		   return {...state, extraTops: action.payload.extra_tops,
+	                         extraBottoms: action.payload.extra_bottoms,
+	                         standaloneTops: action.payload.standalone_tops,
+	                         standaloneBottoms: action.payload.standalone_bottoms,
+	                         suggestedTops: action.payload.suggested_tops,
+	                         suggestedBottoms: action.payload.suggested_bottoms}
 		case ORGANIZE_PIECES:
-			   return Object.assign({}, state, {UpperComponents: action.payload.UpperComponents},
-					     		 			   {LowerComponents: action.payload.LowerComponents})		    
+			   return {...state, UpperComponents: action.payload.UpperComponents,
+					     		 LowerComponents: action.payload.LowerComponents}		    
 		case SET_CONTEMPLATED_PIECE:
-		   return Object.assign({}, state, {contemplated_piece: action.payload.contemplated_piece})
+		   return {...state, contemplated_piece: action.payload.contemplated_piece}
 		default:
 			return state;
 	}
