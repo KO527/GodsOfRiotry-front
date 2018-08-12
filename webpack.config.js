8a4b6273b031a07074323e2366b334c429b1da81
KO527 <Kwajo.oseiakoto@gmail.com>
2018-08-12 00:27:00 -0400

    reverted webpack.config.js to original style

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,14 @@
-const path = require('path');
-const context = path.resolve(__dirname, 'src');
-
-module.exports = {
-  context,
-  entry: './index.js',
+{
   module: {
-    loaders: [
+    rules: [
       {
-        include: path.resolve(__dirname, './src'),
-        loaders: [
-          'style-loader',
-          'css-loader?importLoader=1&modules&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
-        ],
-        test: /\.css$/
-      },
-      {
-        include: path.resolve(__dirname, './src'),
-        loader: 'babel-loader',
-        query: {
-          plugins: [
-            'transform-react-jsx',
-            [
-              'react-css-modules',
-              {
-                context
-              }
+        test: /\.css$/,
+        use: [
+          { loaders: [
+             'style-loader',
+             'css-loader?importLoader=1&modules&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
             ]
-          ]
-        },
-        test: /\.js$/
-      }
+          }
     ]
-  },
-  output: {
-    filename: '[name].js'
-  },
-  stats: 'minimal'
-};
+  }
+}
